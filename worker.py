4d90552bb23894db253af834d70bb7a3c63594a5
Ramsish Mourya <ramshishmourya@gmail.com>
2026-06-22 11:59:32 -0400

    voice chat  app

--- worker.py
+++ worker.py
@@ -4,8 +4,32 @@ import requests
 openai_client = OpenAI()
 
 
+
 def speech_to_text(audio_binary):
-    return None
+
+	# Set up Watson Speech-to-Text HTTP Api url
+	base_url = 'https://sn-watson-stt.labs.skills.network'
+	api_url = base_url+'/speech-to-text/api/v1/recognize'
+
+	# Set up parameters for our HTTP request
+	params = {
+		'model': 'en-US_Multimedia',
+	}
+
+	# Set up the body of our HTTP request
+	body = audio_binary
+
+	# Send a HTTP Post request
+	response = requests.post(api_url, params=params, data=audio_binary).json()
+
+	# Parse the response to get our transcribed text
+	text = 'null'
+	while bool(response.get('results')):
+		print('speech to text response:', response)
+		text = response.get('results').pop().get('alternatives').pop().get('transcript')
+		print('recognised text: ', text)
+		return text
+
 
 
 def text_to_speech(text, voice=""):
@@ -13,4 +37,19 @@ def text_to_speech(text, voice=""):
 
 
 def openai_process_message(user_message):
-    return None
+    # Set the prompt for OpenAI Api
+    prompt = "Act like a personal assistant. You can respond to questions, translate sentences, summarize news, and give recommendations. Keep responses concise - 2 to 3 sentences maximum."
+    # Call the OpenAI Api to process our prompt
+    openai_response = openai_client.chat.completions.create(
+        model="gpt-5-nano", 
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": user_message}
+        ],
+        max_completion_tokens=1000
+    )
+    print("openai response:", openai_response)
+    # Parse the response to get the response message for our prompt
+    response_text = openai_response.choices[0].message.content
+    return response_text
+
